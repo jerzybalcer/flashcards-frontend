ebd309c869039243956280f6c3f67beecf488367
Julka Kubisa <79585810+juliakubisa@users.noreply.github.com>
2024-03-19 19:27:59 +0100

    add input to modal

--- src/components/AddCardModal.tsx
+++ src/components/AddCardModal.tsx
@@ -1,22 +1,38 @@
-import { Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay } from "@chakra-ui/react"
+import { FormControl, FormLabel, Input, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay } from "@chakra-ui/react"
+import {Button} from "@chakra-ui/react"
+import { useState } from "react";
+import {addCard} from "../services/CardService"
 
 interface AddCardModalProps {
-    isOpen: boolean;
+    isOpen: boolean;    
     onClose: () => void;
 }
 
 export const AddCardModal: React.FC<AddCardModalProps> = ({ isOpen, onClose }) => {
+    const [foreignWord, setForeignWord] = useState<string>("")
+    const [translatedWord, setTranslatedWord] = useState<string>("")
+
     return (
         <Modal isOpen={isOpen} onClose={onClose}>
             <ModalOverlay />
             <ModalContent>
-            <ModalHeader>Modal Title</ModalHeader>
+            <ModalHeader>Add new card</ModalHeader>
             <ModalCloseButton />
             <ModalBody>
-                You can add new card here
+                <FormControl>
+                <FormLabel>Foreign Word</FormLabel>
+                <Input placeholder='Enter the word' />
+                </FormControl>
+
+                <FormControl mt={4}>
+                <FormLabel>Translated Word</FormLabel>
+                <Input placeholder='Enter the word' />
+                </FormControl>
             </ModalBody>
 
             <ModalFooter>
+                <Button colorScheme='teal' mr={3} onClick={onClose}> Add Item </Button>
+                <Button variant='ghost' onClick={onClose}> Close </Button>
             </ModalFooter>
             </ModalContent>
         </Modal>
