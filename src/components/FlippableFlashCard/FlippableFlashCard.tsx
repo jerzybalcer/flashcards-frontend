f0c6f208422f06415f0760820f4445418c7d1e2d
Jerzy Balcer <jurekbalcer@gmail.com>
2024-03-31 13:41:17 +0200

    working prototype

--- src/components/FlippableFlashCard/FlippableFlashCard.tsx
+++ src/components/FlippableFlashCard/FlippableFlashCard.tsx
@@ -1,25 +1,52 @@
+import { useLayoutEffect, useRef, useState } from 'react';
 import { Box, Card, Center, Text } from '@chakra-ui/react';
 import { FlashCard } from '../../model/FlashCard'
 import './FlippableFlashCard.css'
-import { useEffect } from 'react';
 
 interface FlippableFlashCardProps {
     flashCard: FlashCard;
 }
 
 export const FlippableFlashCard: React.FC<FlippableFlashCardProps> = ({ flashCard })  => {
+    const textMargin = 8;
+    const maxFontSize = 150;
+    
+    const [fontSize, setFontSize] = useState<number>(maxFontSize);
 
-    useEffect(() => {
+    const textRef = useRef<HTMLParagraphElement>(null);
+    const cardRef = useRef<HTMLDivElement>(null);
+
+    useLayoutEffect(() => {
         document.querySelector('.flip-card')!.classList.remove("flipped");
+        setFontSize(maxFontSize);
     }, [flashCard]);
 
+    useLayoutEffect(() => {
+        scaleDownText();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [fontSize]);
+
+    const handleFlip = () => {
+        document.querySelector('.flip-card')!.classList.add("flipped")
+    };
+
+    const scaleDownText = () => {
+        if(!textRef.current || !cardRef.current) return;
+        
+        if((textRef.current.scrollWidth > cardRef.current.scrollWidth - textMargin) || 
+            (textRef.current.scrollHeight > cardRef.current.scrollHeight - textMargin)){
+            setFontSize(fontSize - 4);
+            return;
+        }
+    };
+
     return (
-    <Box className="flip-card" onClick={() => document.querySelector('.flip-card')!.classList.add("flipped")}>
+    <Box className="flip-card" onClick={() => handleFlip()}>
         <Box className="flip-card-inner">
             <Box className="flip-card-front">
-                <Card w='100%' h='100%'>
+                <Card w='100%' h='100%' ref={cardRef}>
                     <Center h='100%'>
-                        <Text>{flashCard.foreignWord}</Text>
+                        <Text ref={textRef} fontSize={fontSize}>{flashCard.foreignWord}</Text>
                     </Center>
                 </Card>
 
