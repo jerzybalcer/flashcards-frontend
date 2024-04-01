87ea04d0f2235a557c1f64ed8080ca54269dbb27
Jerzy Balcer <jurekbalcer@gmail.com>
2024-04-01 12:01:28 +0200

    texts fits dynamically to container

--- src/components/FittedText.tsx
+++ src/components/FittedText.tsx
@@ -4,31 +4,41 @@ import { useLayoutEffect, useRef, useState } from "react";
 interface FittedTextProps {
     content: string;
     containerRef: React.RefObject<HTMLDivElement>;
-    maxFontSize?: number;
+    minFontSize?: number;
 }
 
-export const FittedText: React.FC<FittedTextProps> = ({content, containerRef, maxFontSize = 150}) => {
-    const [fontSize, setFontSize] = useState<number>(maxFontSize);
+export const FittedText: React.FC<FittedTextProps> = ({content, containerRef, minFontSize = 20}) => {
+    const [fontSize, setFontSize] = useState<number>(0);
+    const [isDoneResizing, setIsDoneResizing] = useState<boolean>(false);
 
     const textRef = useRef<HTMLParagraphElement>(null);
 
-    const scaleDownText = () => {
+    const scaleText = () => {
         if(!textRef.current || !containerRef.current) return;
 
-        if((textRef.current.scrollWidth > containerRef.current.scrollWidth) || 
-            (textRef.current.scrollHeight > containerRef.current.scrollHeight)){
+        // make font size bigger when text is still smaller than container
+        if((textRef.current.scrollWidth <= containerRef.current.scrollWidth) && 
+            (textRef.current.scrollHeight <= containerRef.current.scrollHeight)){
+            setFontSize(fontSize + 4);
+            return;
+        }
+        // when text finally overflows container, make it just a bit smaller to fit
+        else {
             setFontSize(fontSize - 4);
+            setIsDoneResizing(true);
             return;
         }
     };
 
     useLayoutEffect(() => {
-        setFontSize(maxFontSize);
+        setFontSize(minFontSize);
+        setIsDoneResizing(false);
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [content]);
 
     useLayoutEffect(() => {
-        scaleDownText();
+        if(!isDoneResizing)
+            scaleText();
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [fontSize]);
 
