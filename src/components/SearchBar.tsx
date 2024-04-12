e46c4e4b38c34d9a7608a5a69d93b29389a34937
jerzybalcer <jurekbalcer@gmail.com>
2024-04-13 00:16:26 +0200

    clear searchbar button

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,19 +1,33 @@
-import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react"
-import { IconSearch } from "@tabler/icons-react"
+import { Input, InputGroup, InputLeftElement, InputRightElement } from "@chakra-ui/react"
+import { IconSearch, IconX } from "@tabler/icons-react"
+import { useState } from "react";
 
 interface SearchBarProps {
     onSearch: (phrase: string) => void;
 }
 
 export const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
+    const [text, setText] = useState<string>('');
+
+    const handleChange = (newValue: string) => {
+        setText(newValue);
+        onSearch(newValue);
+    };
+
     return (
     <InputGroup margin='auto'>
         <InputLeftElement pointerEvents='none'>
             <IconSearch color='gray' />
         </InputLeftElement>
-        <Input placeholder='Search for a word' 
-            onChange={(ev: React.ChangeEvent<HTMLInputElement>) => onSearch(ev.currentTarget.value)} 
+        <Input placeholder='Search for a word' value={text}
+            onChange={(ev: React.ChangeEvent<HTMLInputElement>) => handleChange(ev.currentTarget.value)}
         />
+        {text.length > 0 &&         
+            <InputRightElement cursor='pointer' onClick={() => handleChange('')}>
+                <IconX color='gray'/>
+            </InputRightElement>
+        }
+
     </InputGroup>
     )
 }
