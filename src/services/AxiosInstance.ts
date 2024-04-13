b9d3bb2b96014edd56f33b17a3a2a7563b23eae2
jerzybalcer <jurekbalcer@gmail.com>
2024-04-13 16:51:54 +0200

    env var accessing update for vite

--- src/services/AxiosInstance.ts
+++ src/services/AxiosInstance.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useCasingInterceptors as setupCasingInterceptors } from "./CasingConverterInterceptor";
 
 export const apiClient = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL ?? "http://127.0.0.1:5000",
+  baseURL: import.meta.env.VITE_API_BASE_URL ?? "http://127.0.0.1:5000",
 });
 
 setupCasingInterceptors();
