6ea07b7d7ac26aaf9140267aae94086ea642fc9a
jerzybalcer <jurekbalcer@gmail.com>
2025-06-16 20:55:17 +0200

    new lang input prototype

--- src/shared/components/LanguageInput.tsx
+++ src/shared/components/LanguageInput.tsx
@@ -1,4 +1,6 @@
-import { Select } from "@chakra-ui/react"
+import { useMemo, useRef, useState } from "react";
+import { Button, Flex, Input, Popover, PopoverBody, PopoverContent, PopoverTrigger, useDisclosure, InputGroup, InputRightElement, Center, useTheme } from "@chakra-ui/react"
+import { IconCheck, IconChevronDown } from "@tabler/icons-react";
 import { Language } from "@/model/Language"
 
 interface Props {
@@ -9,23 +11,94 @@ interface Props {
 }
 
 export const LanguageInput: React.FC<Props> = ({ languages, isLoading, value, onChange }) => {
-    function handleChange(event: React.ChangeEvent<HTMLSelectElement>) {
-        const languageId = event.currentTarget.value;
-        const language = languages.find(l => l.id === languageId);
+    const internalInputRef = useRef<HTMLInputElement>(null);
+    const dropdown = useDisclosure();
 
-        onChange(language);
+    const [inputText, setInputText] = useState<string>('');
+    
+    const blue = useTheme().colors.blue;
+
+    function handleLanguageMouseDown(event: React.MouseEvent<HTMLButtonElement>, language: Language){
+        event.preventDefault();
+
+        let newLanguage: Language | undefined = language;
+
+        if(language.name === value?.name){ // uncheck
+            newLanguage = undefined;
+        }
+
+        if(newLanguage) {
+            dropdown.onClose();
+        }
+
+        onChange(newLanguage);
+        setInputText(newLanguage?.name ?? '');
     }
 
-    return <Select 
-                isDisabled={isLoading} 
-                placeholder="Select language" 
-                value={value?.id} 
-                onChange={handleChange}
-            >
-            {languages.map(language => 
-                <option key={language.id} value={language.id}>
-                    {language.name}
-                </option>
-            )}
-            </Select>
-} 
+    function handleInternalInputChange(event: React.ChangeEvent<HTMLInputElement>){
+        if(!dropdown.isOpen){
+            dropdown.onOpen();
+        }
+        setInputText(event.currentTarget.value);
+    }
+
+    function handleInternalInputBlur(){
+        dropdown.onClose();
+
+        if(value && value.name !== inputText)
+            setInputText(value.name)
+    }
+
+    const filteredLanguages = useMemo(() =>
+        languages.filter((lang) =>
+            lang.name.toLowerCase().startsWith(inputText.toLowerCase())
+        ),
+        [languages, inputText]
+    );
+
+    return <Popover
+            returnFocusOnClose={false}
+            isOpen={dropdown.isOpen}
+            onClose={dropdown.onClose}
+            placement='top'
+            closeOnBlur={true}
+            initialFocusRef={internalInputRef}
+            isLazy
+    >
+        <PopoverTrigger>
+            <InputGroup onClick={dropdown.onOpen}>
+                <Input
+                    ref={internalInputRef}
+                    isDisabled={isLoading}
+                    value={inputText}
+                    placeholder="Pick or enter language"
+                    onChange={handleInternalInputChange}
+                    onBlur={handleInternalInputBlur}
+                />
+                <InputRightElement>
+                    <IconChevronDown />
+                </InputRightElement>
+            </InputGroup>
+        </PopoverTrigger>
+        <PopoverContent maxH='120px' overflowY='auto' bg='gray.700' style={{ scrollbarWidth: 'none'}} border={dropdown.isOpen ? `solid ${blue[300]} 2px` : ''}>
+          <PopoverBody>
+            <Flex direction='column'>
+                {filteredLanguages.length === 0 && <Center p={2} opacity={0.8}>No matching languages</Center>}
+
+                {filteredLanguages.length > 0 && filteredLanguages.map(lang => 
+                    <Button 
+                        w='100%' 
+                        variant='ghost' 
+                        p={2} 
+                        key={lang.id} 
+                        rightIcon={value?.id === lang.id ? <IconCheck color={blue[200]}/> : undefined} 
+                        onMouseDown={(event) => handleLanguageMouseDown(event, lang)}
+                    >
+                        {lang.name}
+                    </Button>
+                )}
+            </Flex>
+          </PopoverBody>
+        </PopoverContent>
+      </Popover>
+}
