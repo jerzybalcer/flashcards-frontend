4f8321d8a9397475204277cee84a577d9e4525b3
jerzybalcer <jurekbalcer@gmail.com>
2026-01-17 22:30:53 +0100

    work in progress

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,30 +1,18 @@
-import { Card, Flex, Heading } from "@chakra-ui/react"
-import { SideMenu } from "@/shared/components/SideMenu"
+import { Flex, Heading } from "@chakra-ui/react"
 import { useAuth } from "@/shared/hooks/general/useAuth";
+import { PageHeading } from "@/shared/components/PageHeading";
 
 export const HomePage = () => {
     const auth = useAuth();
 
     return (
-        <Flex direction='column' h='100%' w='100%' gap={12}>
-            <Flex justify='space-between' align='center' gap={4}>
+        <Flex direction='column' h='100%' w='100%'>
+            <PageHeading title="Flashcards" urlToGoBack={null}/>
+                <Flex justify='space-between' align='center' gap={4}>
                 <Heading size='2xl'>Hello, {auth!.currentUser!.name}</Heading>
-                <SideMenu />
             </Flex>
             <Flex direction='column' px={4} pb={2} flexGrow={1} gap={8}>
-                <Flex flexGrow={1} direction='column' gap={2}>
-                    <Heading size='md'>Recommended</Heading>
-                    <Card borderRadius='md' w='100%' h='100%'></Card>
-                </Flex>
-                <Flex flexGrow={1} direction='column' gap={2}>
-                    <Heading size='md'>Stats</Heading>
-                    <Card borderRadius='md' w='100%' h='100%'></Card>
-                </Flex>
-                <Flex flexGrow={1} direction='column' gap={2}>
-                    <Heading size='md'>Decks</Heading>
-                    <Card borderRadius='md' w='100%' h='100%'></Card>
-                </Flex>
             </Flex>
         </Flex>
-    )
+    );
 }
--- src/shared/components/PageHeading.tsx
+++ src/shared/components/PageHeading.tsx
@@ -1,11 +1,11 @@
-import { Flex, Heading } from "@chakra-ui/react"
+import { Box, Flex, Heading } from "@chakra-ui/react"
 import { SideMenu } from "./SideMenu"
 import { IconArrowLeft } from "@tabler/icons-react";
 import { useNavigate } from "react-router-dom";
 
 interface PageHeadingProps {
     title: string;
-    urlToGoBack?: string;
+    urlToGoBack?: string | null;
 }
 
 export const PageHeading: React.FC<PageHeadingProps> = ({ title, urlToGoBack = '/home' }) => {
@@ -14,7 +14,10 @@ export const PageHeading: React.FC<PageHeadingProps> = ({ title, urlToGoBack = '
     return (
         <Flex w='100%' justify='center' mb={12}>
             <Flex w='100%' maxW='1200px' justify='space-between' align='center'>
-                <IconArrowLeft cursor='pointer' size={36} strokeWidth={1.25} opacity={0.8} onClick={() => navigate(urlToGoBack)}/>
+                {urlToGoBack !== null ? 
+                (<IconArrowLeft cursor='pointer' size={36} strokeWidth={1.25} opacity={0.8} onClick={() => navigate(urlToGoBack)}/>) 
+                : (<Box width='36px' height='36px'/>)
+                }
                 <Heading size='lg'>{title}</Heading>
                 <SideMenu />
             </Flex>
