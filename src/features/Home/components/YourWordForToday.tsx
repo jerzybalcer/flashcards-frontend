6b9cc4734d0a6288f5b0da2f94d2aca3db5ebd56
jerzybalcer <jurekbalcer@gmail.com>
2026-01-20 16:37:56 +0100

    Squashed commit of the following:
    commit fac38aa3d6d6819aa80212d81d827d904040b61c
    Author: jerzybalcer <jurekbalcer@gmail.com>
    Date:   Tue Jan 20 16:35:57 2026 +0100
    
        home screen title improvements
    
    commit 33a9021c58fb1a6f10a13dd4f0b60fbb19cf99cc
    Author: jerzybalcer <jurekbalcer@gmail.com>
    Date:   Tue Jan 20 13:01:01 2026 +0100
    
        homepage ui without api
    
    commit 170b3e985e756517f76b31e34ce9b6c339017bc6
    Author: jerzybalcer <jurekbalcer@gmail.com>
    Date:   Mon Jan 19 00:34:51 2026 +0100
    
        custom fonts
    
    commit ef5c947eafd0646538f02890da1fcbd58b2277ad
    Author: Jerzy <jurekbalcer@gmail.com>
    Date:   Sun Jan 18 20:29:10 2026 +0100
    
        skeleton
    
    commit 4f8321d8a9397475204277cee84a577d9e4525b3
    Author: jerzybalcer <jurekbalcer@gmail.com>
    Date:   Sat Jan 17 22:30:53 2026 +0100
    
        work in progress

--- src/features/Home/components/YourWordForToday.tsx
+++ src/features/Home/components/YourWordForToday.tsx
@@ -5,8 +5,8 @@ export const YourWordForToday = () => {
     return (
         <Flex direction="column" gap={5}>
             <Flex justify='space-between' align='center'>
-                <Heading fontSize='24px' fontFamily='Playwrite US Modern' fontWeight={400}>Your word for today</Heading>
-                <IconInfoCircle size='36px' strokeWidth={1.5} color="var(--chakra-colors-blue-200)"/>
+                <Heading fontSize='24px' fontFamily='Playwrite US Modern' fontWeight={400}>Your words for today</Heading>
+                <IconInfoCircle size='32px' strokeWidth={1.5} color="var(--chakra-colors-blue-200)"/>
             </Flex>
             <Card onClick={() => {}}>
                 <CardBody gap={2} display='flex' flexDirection='column' justifyContent='center' alignItems='center'>
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,4 @@
-import { Flex, Heading } from "@chakra-ui/react"
-import { useAuth } from "@/shared/hooks/general/useAuth";
+import { Flex } from "@chakra-ui/react"
 import { PageHeading } from "@/shared/components/PageHeading";
 import { RecentDecks } from "@/features/Home/components/RecentDecks";
 import { YourWordForToday } from "@/features/Home/components/YourWordForToday";
@@ -7,19 +6,15 @@ import { YourGoals } from "@/features/Home/components/YourGoals";
 import { Scrollable } from "@/shared/components/Scrollable";
 
 export const HomePage = () => {
-    const auth = useAuth();
 
     return (
         <Flex direction='column' h='100%' w='100%'>
-            <PageHeading title="Cardify" urlToGoBack={null}/>
+            <PageHeading title="Dashboard" urlToGoBack={null}/>
             <Scrollable>
-                <Flex direction='column' px={1} gap={6}>
-                    <Heading fontSize='36px' fontFamily='Playwrite US Modern' fontWeight={400}>Hello, {auth!.currentUser!.name}</Heading>
-                    <Flex direction='column' gap={10}>
-                        <YourWordForToday />
-                        <YourGoals />
-                        <RecentDecks />
-                    </Flex>
+                <Flex direction='column' px={1} gap={10}>
+                    <YourWordForToday />
+                    <YourGoals />
+                    <RecentDecks />
                 </Flex>
 
             </Scrollable>
