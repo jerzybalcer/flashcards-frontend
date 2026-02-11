5157ca5ae6e6993b8c90c8f132445b6edf9c6445
jerzybalcer <jurekbalcer@gmail.com>
2026-02-11 19:33:54 +0100

    slight sidemenu redesign

--- src/shared/components/SideMenu.tsx
+++ src/shared/components/SideMenu.tsx
@@ -1,5 +1,5 @@
-import { Avatar, Box, Drawer, DrawerBody, DrawerCloseButton, DrawerContent, DrawerHeader, DrawerOverlay, Flex, Heading, Text, useDisclosure } from "@chakra-ui/react"
-import { IconMenu2 } from "@tabler/icons-react";
+import { Avatar, Box, Button, Drawer, DrawerBody, DrawerCloseButton, DrawerContent, DrawerHeader, DrawerOverlay, Flex, Heading, Text, useDisclosure } from "@chakra-ui/react"
+import { IconLogout, IconMenu2 } from "@tabler/icons-react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../hooks/general/useAuth";
 
@@ -21,34 +21,26 @@ export const SideMenu: React.FC = () => {
                             <Flex align='center' gap={4}>
                                 <Avatar src={auth?.currentUser?.imageUrl} name={auth?.currentUser?.name}/>
                                 <Box minW={0} pr={4}>
-                                    <Heading size='md' textOverflow='ellipsis' whiteSpace='nowrap' overflow='hidden'>{auth?.currentUser?.name}</Heading>
-                                    <Text opacity={0.8} fontSize='md'>{auth?.currentUser?.email}</Text>
+                                    <Heading fontSize='t1' as='h1' textOverflow='ellipsis' whiteSpace='nowrap' overflow='hidden'>{auth?.currentUser?.name}</Heading>
+                                    <Text opacity={0.8} fontSize='t2'>{auth?.currentUser?.email}</Text>
                                 </Box>
                             </Flex>
   
                         </DrawerHeader>
                         <DrawerBody>
-                            <Flex direction='column' gap={4}>
-                                <Heading as='h2' size='lg'
-                                    onClick={() => navigate('/home')}>
-                                    Home
-                                </Heading>
-                                <Heading as='h2' size='lg'
-                                    onClick={() => navigate('/decks')}>
+                            <Flex direction='column' gap={8} pt={4}>
+                                <Button variant='link' onClick={() => navigate('/home')} borderRadius='xl' size='lg' fontSize='h2'>
+                                    Dashboard
+                                </Button>
+                                <Button variant='link' onClick={() => navigate('/decks')} borderRadius='xl' size='lg' fontSize='h2'>
                                     Decks
-                                </Heading>
-                                <Heading as='h2' size='lg'
-                                    onClick={() => navigate('/stats')}>
+                                </Button>
+                                <Button variant='link' onClick={() => navigate('/stats')} borderRadius='xl' size='lg' fontSize='h2'>
                                     Stats
-                                </Heading>
-                                <Heading as='h2' size='lg' mt={8}
-                                    onClick={() => {}}>
-                                    Settings
-                                </Heading>
-                                <Heading as='h2' size='lg'
-                                    onClick={() => auth?.logout()}>
+                                </Button>
+                                <Button variant='ghost' colorScheme='red' onClick={() => auth?.logout()} borderRadius='xl' size='lg' fontSize='h2' rightIcon={<IconLogout />}>
                                     Logout
-                                </Heading>
+                                </Button>
                             </Flex>
                         </DrawerBody>
                     </DrawerContent>
